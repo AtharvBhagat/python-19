0843c474c5b38d294ca6757eea1c65aa522cb2db
AtharvBhagat <159274676+AtharvBhagat@users.noreply.github.com>
2025-04-24 10:04:04 +0530

    Add files via upload

---
+++ python-19.py
@@ -0,0 +1,10 @@
+def printnumber(n):
+    i=0
+    print("the number ernter by user is ", n)
+    i=i+1
+    print("theThe total iteration done by code is ",i)
+
+printnumber(90)
+printnumber(250)
+
+print("With any number entered by the user the time taken to run the code won't change")
